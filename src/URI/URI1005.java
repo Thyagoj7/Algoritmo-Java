da180e67b18d3aaf79bc0c5e107df1868f7bbb72
Thyago <t.y_josef7@hotmail.com>
2021-01-17 17:19:24 -0300

    Projeto 1005

--- src/URI/URI1005.java
+++ src/URI/URI1005.java
@@ -1,9 +1,28 @@
 package URI;
 
+import java.util.Scanner;
+
 public class URI1005 {
     public static void main(String[] args) {
         double A;
         double B;
+        double MEDIA;
+
+        Scanner ler = new Scanner (System.in);
+        A = ler.nextDouble();
+        B = ler.nextDouble();
+
+
+        A=35.0/A;
+        B=75.0/B;
+
+        MEDIA=(A+B)/2;
+
+       // System.out.printf("%.1f",A);
+        //System.out.printf("%.1f",B);
+
+        System.out.printf("MEDIA = " + "%.5f",MEDIA);
+
 
     }
 }
